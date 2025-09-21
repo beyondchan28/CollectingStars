73068d642bcc4b088b42c177c4a7464658e6a058
beyondchan28 <beyonddd.games@gmail.com>
2025-09-21 18:32:13 +0700

    right click launch done

--- Scripts/star.gd
+++ Scripts/star.gd
@@ -1,5 +1,8 @@
 class_name Star extends RigidBody2D
 
+
+const LAUNCH_POWER := 500.0
+
 enum StarType {
 	RED, GREEN, BLUE
 }
@@ -42,3 +45,8 @@ func set_type(st: StarType) -> void:
 	_type = st
 func get_type() -> StarType:
 	return _type
+
+func launch() -> void:
+	self.apply_impulse(Vector2.LEFT * LAUNCH_POWER) # launch projectile
+	await get_tree().create_timer(0.3).timeout
+	self.apply_impulse(Vector2.RIGHT * (LAUNCH_POWER - 10.0)) # cancel out the launch impulse after 0.3 secs
--- Scripts/star_button.gd
+++ Scripts/star_button.gd
@@ -1,5 +1,6 @@
 class_name StarButton extends TextureButton
 
+
 @onready var _stars_parent: Node2D = $"../../../../StarsParent"
 
 @export var _star_amount := 1 # how many star this button contain
@@ -8,24 +9,33 @@ class_name StarButton extends TextureButton
 const _star := preload("res://Scenes/star.tscn")
 
 func _ready() -> void:
+	self.pivot_offset = self.size/2.0
 	_set_label()
 	$AmountLabel.text = str(_star_amount)
 	self.pressed.connect(_on_pressed)
 	
 func _on_pressed() -> void:
-	var _new_star :Star = _star.instantiate()
-	_new_star.set_type(_star_type)
-	_new_star.freeze_physics(false)
-	MouseState.set_drag(true)
-	MouseState.set_holded_star(_new_star)
-	_stars_parent.add_child(_new_star)
-	_new_star.global_position = get_global_mouse_position()
-	decrease_star_amount()
-	if _star_amount == 0:
-		self.queue_free()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var _new_star :Star = _star.instantiate()
+		_new_star.set_type(_star_type)
+		# make the mouse state to dragging and holding star
+		_new_star.freeze_physics(false)
+		MouseState.set_drag(true)
+		MouseState.set_holded_star(_new_star)
+		_new_star.global_position = get_global_mouse_position()
+		# ------------------------------------------------------
+		_stars_parent.add_child(_new_star)
+		decrease_star_amount()
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		var _new_star :Star = _star.instantiate()
+		_new_star.set_type(_star_type)
+		_new_star.global_position = self.global_position + self.size/2.0 # launch from the middle of the button
+		_stars_parent.add_child(_new_star)
+		_new_star.launch() # launch star
+		decrease_star_amount()
+		
 
 func _set_label() -> void :
-	print(_star_type)
 	match _star_type:
 		Star.StarType.RED:
 			$TypeLabel.text = "RED"
@@ -41,6 +51,8 @@ func increase_star_amount() -> void:
 func decrease_star_amount() -> void:
 	_star_amount -= 1
 	$AmountLabel.text = str(_star_amount)
+	if _star_amount == 0:
+		self.queue_free()
 
 func set_star_type(s: Star.StarType) -> void:
 	_star_type = s
